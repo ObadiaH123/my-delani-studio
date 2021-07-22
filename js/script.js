14382060e718216d2c04a07eec8c7f4cc2fcc771
obadiaH123 <benard.bett@student.moringaschool.com>
2021-07-23 00:05:09 +0300

    updating the codes for form in js and in css

--- js/script.js
+++ js/script.js
@@ -74,17 +74,15 @@ $("#col-1").hover(function() {
       $("#photo8").fadeTo("slow",1);
     });
 // validation of form
-        $("form#form").on('submit',function(event){
-            event.preventDefault();
-            let name = $("input#name").val();
-            let email = $("input#email").val();
-            let message = $("textarea#message").val();
-
-            if ($("input#name").val() && $("input#email").val()){
-                alert ("Hey " + name + ", Thanks for reaching out, we'll get back to you shortly...cheers");
-            }
-            else {
-                alert("Please provide your correct name and email!");
-            }
-
-        });
+        // giving feedback to the customer
+    $("button").click(function () {
+        var customer = document.getElementById('nameDetail').value;
+        alert(' Dear ' + ' esteemed ' + customer + ' we have received your message successful. ' + ' Feel free to reach out to us anytime');
+        // omonge.preventDefault();
+    });
+    //    reset my form button
+    // $("button").on('click', function () {
+    //     $('form').each(function () {
+    //     this.reset();
+    //     });
+    // });
