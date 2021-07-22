706479e7995f458b1e817ed3ab018a8238948b0f
obadiaH123 <benard.bett@student.moringaschool.com>
2021-07-23 00:24:03 +0300

    adding the JS validation

--- js/script.js
+++ js/script.js
@@ -74,15 +74,50 @@ $("#col-1").hover(function() {
       $("#photo8").fadeTo("slow",1);
     });
 // validation of form
-        // giving feedback to the customer
-    $("button").click(function () {
-        var customer = document.getElementById('nameDetail').value;
-        alert(' Dear ' + ' esteemed ' + customer + ' we have received your message successful. ' + ' Feel free to reach out to us anytime');
-        // omonge.preventDefault();
+function ValidateEmail(email) {
+    var expr = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+    return expr.test(email);
+  };
+  
+  $(document).ready(function(){
+    $("form#delani-studio").submit(function(event){
+      var name = $("input#name").val();
+      var email = $("input#email").val();
+      var message = $("textarea#message").val();
+  
+      if (!$("input#name").val()){
+        alert("Please enter your name!")
+      }
+      else if (!$("input#email").val()){
+        alert("Please enter your email!");
+      }
+  
+      if (!ValidateEmail($("input#email").val())) {
+        alert("Invalid email address!");
+      }
+      else if (!$("textarea#message").val()){
+        alert("Please enter your message or comment!");
+      }
+      else{
+        alert (name + ", we have received your message. Thank you for reaching out to us.");
+      }
+      
+      $('form#delani-studio').get(0).reset();
+      return false;
+      
     });
-    //    reset my form button
-    // $("button").on('click', function () {
-    //     $('form').each(function () {
-    //     this.reset();
-    //     });
+  
+  });
+  
+    //     // giving feedback to the customer
+    // $("button").click(function () {
+    //     var customer = document.getElementById('nameDetail').value;
+    //     alert(' Dear ' + ' esteemed ' + customer + ' we have received your message successful. ' + ' Feel free to reach out to us anytime');
+    //     // omonge.preventDefault();
     // });
+    // //    reset my form button
+    // // $("button").on('click', function () {
+    // //     $('form').each(function () {
+    // //     this.reset();
+    // //     });
+    // // });
